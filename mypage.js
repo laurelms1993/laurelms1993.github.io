c3a3ec09decb6d2a080855e7a2e1c423d6404aec
Laurel Michel-Schottman <laurel@Laurels-MBP-2.wv.cc.cmu.edu>
2015-12-12 20:10:52 -0500

    more people

--- mypage.js
+++ mypage.js
@@ -49,8 +49,157 @@ angular.module("tinderVotes",[])
   		var image = $($('.main_image')[person]).css("opacity", "0");}, 200);
 
   };
-  $scope.randoms = 
   $scope.people =[
+		{"first":"./images/P1/IMG_NEW_2367.PNG","rest":["./images/P1/IMG_NEW_2367.PNG","./images/P1/IMG_NEW_2368.PNG",
+		"./images/P1/IMG_NEW_2369.PNG","./images/P1/IMG_NEW_2370.PNG",]},
+
+		{"first":"./images/P2/IMG_NEW_2363.PNG","rest":["./images/P2/IMG_NEW_2363.PNG","./images/P2/IMG_NEW_2364.PNG", 
+		"./images/P2/IMG_NEW_2365.PNG","./images/P2/IMG_NEW_2366.PNG"]},
+
+		{"first":"./images/P3/IMG_NEW_2357.PNG","rest":["./images/P3/IMG_NEW_2357.PNG","./images/P3/IMG_NEW_2358.PNG",
+		"./images/P3/IMG_NEW_2359.PNG","./images/P3/IMG_NEW_2361.PNG","./images/P3/IMG_NEW_2362.PNG"]},
+
+		{"first":"./images/P4/IMG_NEW_2353.PNG","rest":["./images/P4/IMG_NEW_2353.PNG","./images/P4/IMG_NEW_2354.PNG",
+		"./images/P4/IMG_NEW_2355.PNG","./images/P4/IMG_NEW_2356.PNG"]}, 
+
+		{"first":"./images/P5/IMG_NEW_2347.PNG","rest":["./images/P5/IMG_NEW_2347.PNG","./images/P5/IMG_NEW_2348.PNG",
+		"./images/P5/IMG_NEW_2349.PNG","./images/P5/IMG_NEW_2350.PNG","./images/P5/IMG_NEW_2351.PNG", "./images/P5/IMG_NEW_2352.PNG"]}, 
+
+		{"first":"./images/P6/IMG_NEW_2341.PNG","rest":["./images/P6/IMG_NEW_2341.PNG","./images/P6/IMG_NEW_2342.PNG",
+		"./images/P6/IMG_NEW_2343.PNG","./images/P6/IMG_NEW_2344.PNG","./images/P6/IMG_NEW_2345.PNG","./images/P6/IMG_NEW_2346.PNG"]}, 
+
+		{"first":"./images/P7/IMG_NEW_2335.PNG","rest":["./images/P7/IMG_NEW_2335.PNG","./images/P7/IMG_NEW_2336.PNG",
+		"./images/P7/IMG_NEW_2337.PNG","./images/P7/IMG_NEW_2338.PNG","./images/P7/IMG_NEW_2339.PNG", "./images/P7/IMG_NEW_2340.PNG"]},
+
+		{"first":"./images/P8/IMG_NEW_2325.PNG","rest":["./images/P8/IMG_NEW_2325.PNG","./images/P8/IMG_NEW_2326.PNG",
+		"./images/P8/IMG_NEW_2327.PNG","./images/P8/IMG_NEW_2328.PNG","./images/P8/IMG_NEW_2330.PNG"]}, 
+
+		{"first":"./images/P9/IMG_NEW_2331.PNG","rest":["./images/P9/IMG_NEW_2331.PNG","./images/P9/IMG_NEW_2332.PNG",
+		"./images/P9/IMG_NEW_2333.PNG","./images/P9/IMG_NEW_2334.PNG"]}, 
+
+		{"first":"./images/P10/IMG_NEW_2321.PNG","rest":["./images/P10/IMG_NEW_2321.PNG","./images/P10/IMG_NEW_2322.PNG",
+		"./images/P10/IMG_NEW_2323.PNG","./images/P10/IMG_NEW_2324.PNG"]},
+
+		{"first":"./images/P11/IMG_NEW_2315.PNG","rest":["./images/P11/IMG_NEW_2315.PNG","./images/P11/IMG_NEW_2316.PNG",
+		"./images/P11/IMG_NEW_2317.PNG","./images/P11/IMG_NEW_2318.PNG","./images/P11/IMG_NEW_2319.PNG","./images/P11/IMG_NEW_2320.PNG"]},
+
+		{"first":"./images/P12/IMG_NEW_2309.PNG","rest":["./images/P12/IMG_NEW_2309.PNG","./images/P12/IMG_NEW_2310.PNG",
+		"./images/P12/IMG_NEW_2311.PNG","./images/P12/IMG_NEW_2313.PNG","./images/P12/IMG_NEW_2314.PNG", "./images/P12/6.PNG"]},
+
+		{"first":"./images/P13/IMG_NEW_2306.PNG","rest":["./images/P13/IMG_NEW_2306.PNG","./images/P13/IMG_NEW_2307.PNG",
+		"./images/P13/IMG_NEW_2308.PNG"]},
+
+		{"first":"./images/P14/IMG_NEW_2127.PNG","rest":["./images/P14/IMG_NEW_2127.PNG","./images/P14/IMG_NEW_2128.PNG",
+		"./images/P14/IMG_NEW_2129.PNG"]},
+
+		{"first":"./images/P15/IMG_NEW_2301.PNG","rest":["./images/P15/IMG_NEW_2301.PNG","./images/P15/IMG_NEW_2302.PNG",
+		"./images/P15/IMG_NEW_2303.PNG","./images/P15/IMG_NEW_2304.PNG","./images/P15/IMG_NEW_2305.PNG"]},
+
+		{"first":"./images/P16/IMG_NEW_2297.PNG","rest":["./images/P16/IMG_NEW_2297.PNG","./images/P16/IMG_NEW_2298.PNG",
+		"./images/P16/IMG_NEW_2299.PNG","./images/P16/IMG_NEW_2300.PNG"]},
+
+		{"first":"./images/P17/IMG_NEW_2294.PNG","rest":["./images/P17/IMG_NEW_2294.PNG","./images/P17/IMG_NEW_2295.PNG",
+		"./images/P17/IMG_NEW_2296.PNG"]},
+
+		{"first":"./images/P18/IMG_NEW_2289.PNG","rest":["./images/P18/IMG_NEW_2289.PNG","./images/P18/IMG_NEW_2290.PNG",
+		"./images/P18/IMG_NEW_2291.PNG","./images/P18/IMG_NEW_2292.PNG","./images/P18/IMG_NEW_2293.PNG"]},
+
+		{"first":"./images/P19/IMG_NEW_2285.PNG","rest":["./images/P19/IMG_NEW_2285.PNG","./images/P19/IMG_NEW_2286.PNG"
+		,"./images/P19/IMG_NEW_2287.PNG","./images/P19/IMG_NEW_2288.PNG"]},
+
+		{"first":"./images/P20/IMG_NEW_2281.PNG","rest":["./images/P20/IMG_NEW_2281.PNG","./images/P20/IMG_NEW_2282.PNG",
+		"./images/P20/IMG_NEW_2283.PNG", "./images/P20/IMG_NEW_2284.PNG"]},
+
+		{"first":"./images/P21/IMG_NEW_2275.PNG","rest":["./images/P21/IMG_NEW_2275.PNG","./images/P21/IMG_NEW_2276.PNG",
+		"./images/P21/IMG_NEW_2277.PNG", "./images/P21/IMG_NEW_2278.PNG", "./images/P21/IMG_NEW_2279.PNG", "./images/P21/IMG_NEW_2280.PNG"]},
+
+		{"first":"./images/P22/IMG_NEW_2271.PNG","rest":["./images/P22/IMG_NEW_2271.PNG","./images/P22/IMG_NEW_2272.PNG",
+		"./images/P22/IMG_NEW_2273.PNG","./images/P22/IMG_NEW_2274.PNG"]},
+
+		{"first":"./images/P23/IMG_NEW_2266.PNG","rest":["./images/P23/IMG_NEW_2266.PNG","./images/P23/IMG_NEW_2267.PNG",
+		 "./images/P23/IMG_NEW_2269.PNG", "./images/P23/IMG_NEW_2270.PNG"]},
+
+		{"first":"./images/P24/IMG_NEW_2259.PNG","rest":["./images/P24/IMG_NEW_2259.PNG","./images/P24/IMG_NEW_2260.PNG",
+		"./images/P24/IMG_NEW_2261.PNG", "./images/P24/IMG_NEW_2262.PNG", "./images/P24/IMG_NEW_2263.PNG", "./images/P24/IMG_NEW_2264.PNG"]},
+
+		{"first":"./images/P25/IMG_NEW_2252.PNG","rest":["./images/P25/IMG_NEW_2252.PNG","./images/P25/IMG_NEW_2253.PNG",
+		"./images/P25/IMG_NEW_2254.PNG","./images/P25/IMG_NEW_2256.PNG","./images/P25/IMG_NEW_2257.PNG"]},
+
+		{"first":"./images/P26/IMG_NEW_2249.PNG","rest":["./images/P26/IMG_NEW_2249.PNG","./images/P26/IMG_NEW_2250.PNG",
+		"./images/P26/IMG_NEW_2251.PNG"]},
+
+		{"first":"./images/P27/IMG_NEW_2245.PNG","rest":["./images/P27/IMG_NEW_2245.PNG","./images/P27/IMG_NEW_2246.PNG",
+		"./images/P27/IMG_NEW_2247.PNG", "./images/P27/IMG_NEW_2248.PNG"]},
+
+		{"first":"./images/P28/IMG_NEW_2240.PNG","rest":["./images/P28/IMG_NEW_2241.PNG","./images/P28/IMG_NEW_2242.PNG",
+		"./images/P28/IMG_NEW_2243.PNG", "./images/P28/IMG_NEW_2244.PNG"]},
+
+		{"first":"./images/P29/IMG_NEW_2237.PNG","rest":["./images/P29/IMG_NEW_2237.PNG","./images/P29/IMG_NEW_2238.PNG",
+		"./images/P29/IMG_NEW_2239.PNG"]},
+
+		{"first":"./images/P30/IMG_NEW_2231.PNG","rest":["./images/P30/IMG_NEW_2231.PNG","./images/P30/IMG_NEW_2232.PNG",
+		"./images/P30/IMG_NEW_2233.PNG", "./images/P30/IMG_NEW_2234.PNG", "./images/P30/IMG_NEW_2235.PNG", "./images/P30/IMG_NEW_2236.PNG"]},
+
+		{"first":"./images/P31/IMG_NEW_2228.PNG","rest":["./images/P31/IMG_NEW_2228.PNG","./images/P31/IMG_NEW_2229.PNG",
+		"./images/P31/IMG_NEW_2230.PNG"]},
+
+		{"first":"./images/P32/IMG_NEW_2223.PNG","rest":["./images/P32/IMG_NEW_2223.PNG","./images/P32/IMG_NEW_2224.PNG",
+		"./images/P32/IMG_NEW_2225.PNG", "./images/P32/IMG_NEW_2226.PNG", "./images/P32/IMG_NEW_2227.PNG"]},
+
+		{"first":"./images/P33/IMG_NEW_2219.PNG","rest":["./images/P33/IMG_NEW_2219.PNG","./images/P33/IMG_NEW_2220.PNG",
+		"./images/P33/IMG_NEW_2221.PNG", "./images/P33/IMG_NEW_2222.PNG"]},
+
+		{"first":"./images/P34/IMG_NEW_2212.PNG","rest":["./images/P34/IMG_NEW_2212.PNG","./images/P34/IMG_NEW_2213.PNG",
+		"./images/P34/IMG_NEW_2214.PNG", "./images/P34/IMG_NEW_2215.PNG", "./images/P34/IMG_NEW_2216.PNG", "./images/P34/IMG_NEW_2218.PNG"]},
+
+		{"first":"./images/P35/IMG_NEW_2206.PNG","rest":["./images/P35/IMG_NEW_2206.PNG","./images/P35/IMG_NEW_2207.PNG",
+		"./images/P35/IMG_NEW_2208.PNG", "./images/P35/IMG_NEW_2209.PNG", "./images/P35/IMG_NEW_2210.PNG", "./images/P35/IMG_NEW_2211.PNG"]},
+
+		{"first":"./images/P36/IMG_NEW_2200.PNG","rest":["./images/P36/IMG_NEW_2200.PNG","./images/P36/IMG_NEW_2201.PNG"
+		,"./images/P36/IMG_NEW_2202.PNG","./images/P36/IMG_NEW_2203.PNG","./images/P36/IMG_NEW_2204.PNG","./images/P36/IMG_NEW_2205.PNG"]},
+
+		{"first":"./images/P37/IMG_NEW_2195.PNG","rest":["./images/P37/IMG_NEW_2195.PNG","./images/P37/IMG_NEW_2196.PNG",
+		"./images/P37/IMG_NEW_2197.PNG", "./images/P37/IMG_NEW_2198.PNG", "./images/P37/IMG_NEW_2199.PNG"]},
+
+		{"first":"./images/P38/IMG_NEW_2191.PNG","rest":["./images/P38/IMG_NEW_2191.PNG","./images/P38/IMG_NEW_2192.PNG",
+		"./images/P38/IMG_NEW_2193.PNG", "./images/P38/IMG_NEW_2194.PNG"]},	
+
+		{"first":"./images/P39/IMG_NEW_2185.PNG","rest":["./images/P39/IMG_NEW_2185.PNG","./images/P39/IMG_NEW_2186.PNG",
+		"./images/P39/IMG_NEW_2187.PNG", "./images/P39/IMG_NEW_2188.PNG", "./images/P39/IMG_NEW_2189.PNG", "./images/P39/IMG_NEW_2190.PNG"]},			
+
+		{"first":"./images/P40/IMG_NEW_2182.PNG","rest":["./images/P40/IMG_NEW_2182.PNG","./images/P40/IMG_NEW_2183.PNG",
+		"./images/P40/IMG_NEW_2184.PNG"]},	
+
+		{"first":"./images/P41/IMG_NEW_2176.PNG","rest":["./images/P41/IMG_NEW_2176.PNG","./images/P41/IMG_NEW_2177.PNG"
+		,"./images/P41/IMG_NEW_2178.PNG","./images/P41/IMG_NEW_2179.PNG","./images/P41/IMG_NEW_2180.PNG","./images/P41/IMG_NEW_2181.PNG"]},
+
+		{"first":"./images/P42/IMG_NEW_2171.PNG","rest":["./images/P42/IMG_NEW_2171.PNG","./images/P42/IMG_NEW_2172.PNG",
+		"./images/P42/IMG_NEW_2173.PNG", "./images/P42/IMG_NEW_2174.PNG", "./images/P42/IMG_NEW_2175.PNG"]},
+
+		{"first":"./images/P43/IMG_NEW_2159.PNG","rest":["./images/P43/IMG_NEW_2159.PNG","./images/P43/IMG_NEW_2160.PNG",
+		"./images/P43/IMG_NEW_2161.PNG", "./images/P43/IMG_NEW_2162.PNG", "./images/P43/IMG_NEW_2163.PNG"]},
+
+		{"first":"./images/P44/IMG_NEW_2165.PNG","rest":["./images/P44/IMG_NEW_2165.PNG","./images/P44/IMG_NEW_2166.PNG",
+		"./images/P44/IMG_NEW_2167.PNG", "./images/P44/IMG_NEW_2168.PNG", "./images/P44/IMG_NEW_2169.PNG", "./images/P44/IMG_NEW_2170.PNG"]},	
+
+		{"first":"./images/P45/IMG_NEW_2155.PNG","rest":["./images/P45/IMG_NEW_2155.PNG","./images/P45/IMG_NEW_2156.PNG",
+		"./images/P45/IMG_NEW_2157.PNG", "./images/P45/IMG_NEW_2158.PNG"]},
+
+		{"first":"./images/P46/IMG_NEW_2150.PNG","rest":["./images/P46/IMG_NEW_2150.PNG","./images/P46/IMG_NEW_2151.PNG",
+		"./images/P46/IMG_NEW_2152.PNG", "./images/P46/IMG_NEW_2153.PNG", "./images/P46/IMG_NEW_2154.PNG"]},
+
+		{"first":"./images/P47/IMG_NEW_2144.PNG","rest":["./images/P47/IMG_NEW_2144.PNG","./images/P47/IMG_NEW_2145.PNG",
+		"./images/P47/IMG_NEW_2146.PNG", "./images/P47/IMG_NEW_2147.PNG", "./images/P47/IMG_NEW_2148.PNG", "./images/P47/IMG_NEW_2149.PNG"]},	
+
+		{"first":"./images/P48/IMG_NEW_2138.PNG","rest":["./images/P48/IMG_NEW_2138.PNG","./images/P48/IMG_NEW_2139.PNG",
+		"./images/P48/IMG_NEW_2140.PNG", "./images/P48/IMG_NEW_2141.PNG", "./images/P48/IMG_NEW_2142.PNG", "./images/P48/IMG_NEW_2143.PNG"]},	
+
+		{"first":"./images/P49/IMG_NEW_2130.PNG","rest":["./images/P49/IMG_NEW_2130.PNG","./images/P49/IMG_NEW_2131.PNG",
+		"./images/P49/IMG_NEW_2132.PNG", "./images/P49/IMG_NEW_2133.PNG"]},				
+
+	];
+  
+  $scope.people2 =[
 		{"first":"./images/P1/1.PNG","rest":["./images/P1/1.PNG","./images/P1/2.PNG"]},
 
 		{"first":"./images/P2/1.PNG","rest":["./images/P2/1.PNG","./images/P2/2.PNG", 
